52950b11e0ad7b257f3583462602a79e2b822938
ericKwon95 <ericyong95@gmail.com>
2025-03-25 00:05:41 +0900

    add: 프로젝트 생성

---
+++ ProjectVision/ProjectVision/ProjectVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectVisionApp.swift
+//  ProjectVision
+//
+//  Created by 권승용 on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
