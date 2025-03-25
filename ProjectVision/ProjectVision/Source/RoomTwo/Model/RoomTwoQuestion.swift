0a49fbe1dca85aeee2ada890a6fc329e70081556
mint3382 <mskim0926@naver.com>
2025-03-25 19:37:10 +0900

    feat: RoomTwoView 구현

---
+++ ProjectVision/ProjectVision/Source/RoomTwo/Model/RoomTwoQuestion.swift
@@ -0,0 +1,27 @@
+//
+//  RoomTwoQuestion.swift
+//  ProjectVision
+//
+//  Created by minsong kim on 3/25/25.
+//
+
+enum RoomTwoQuestion {
+    case first
+
+    // MARK: - Computed Properties
+
+    var description: String {
+        switch self {
+        case .first:
+            """
+            강의실에서 수업을 한 귀로 듣고 한 귀로 흘리고 있는데,
+            지난 번에 공간을 열 수 있는 답을 적어주었던 친구와 눈이 마주쳤다.
+
+            아, 어떻게든 저 친구를 우리 동아리에 데려와야 하는데!
+            일단 반갑게 인사를 해야겠다.
+
+            단, 수업 중이니 입모양으로만 전달해보자.
+            """
+        }
+    }
+}
